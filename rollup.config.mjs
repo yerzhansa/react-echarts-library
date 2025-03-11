b7101b078d1ac96d456ba94ff7b4f45d3e2d307c
yerzhansa <yerzhan.st@gmail.com>
2025-03-12 00:28:58 +0500

    Prepare for npm publishing

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,38 +2,51 @@ import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { dts } from "rollup-plugin-dts";
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: true,
-      exports: 'named',
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        echarts: 'echarts'
+const config = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          echarts: 'echarts'
+        }
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+        exports: 'named',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          echarts: 'echarts'
+        }
       }
-    },
-    {
-      file: 'dist/index.esm.js',
-      format: 'esm',
-      sourcemap: true,
-      exports: 'named',
-      globals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        echarts: 'echarts'
-      }
-    }
-  ],
-  external: ['react', 'react-dom', 'react/jsx-runtime', 'echarts'],
-  plugins: [
-    peerDepsExternal(),
-    typescript(),
-    resolve(),
-    commonjs()
-  ]
-};
+    ],
+    external: ['react', 'react-dom', 'react/jsx-runtime', 'echarts'],
+    plugins: [
+      peerDepsExternal(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        exclude: ["node_modules", "example"]
+      }),
+      resolve(),
+      commonjs()
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()]
+  }
+];
+
+export default config;
